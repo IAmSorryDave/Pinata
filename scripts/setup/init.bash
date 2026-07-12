746ff4ca0966bc98163b80720648e2a8391e97f0
IAmSorryDave <212933284+IAmSorryDave@users.noreply.github.com>
2026-07-12 14:41:42 -0700

    Create init.bash for setup script execution
    Initialize setup script to run apt-update and install scripts with appropriate permissions.

---
+++ scripts/setup/init.bash
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+
+chmod +x .github/scripts/setup/apt-update.bash && .github/scripts/setup/apt-update.bash && chmod -x .github/scripts/setup/apt-update.bash && sleep 3
+chmod +x .github/scripts/setup/install.bash && .github/scripts/setup/install.bash && chmod -x .github/scripts/setup/install.bash && sleep 3
