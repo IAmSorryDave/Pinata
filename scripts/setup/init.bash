4fd09c6fe4d437507cf8b94093ed8f5646bc2bcd
IAmSorryDave <212933284+IAmSorryDave@users.noreply.github.com>
2026-07-14 15:14:28 -0700

    Update init.bash

--- scripts/setup/init.bash
+++ scripts/setup/init.bash
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
 chmod +x scripts/setup/apt-update.bash && scripts/setup/apt-update.bash && chmod -x scripts/setup/apt-update.bash && sleep 3
-chmod +x scripts/setup/install.bash && scripts/setup/install.bash && chmod -x scripts/setup/install.bash && sleep 3
+chmod +x scripts/setup/install.bash && scripts/setup/install-dependencies.bash && chmod -x scripts/setup/install-dependencies.bash && sleep 3
