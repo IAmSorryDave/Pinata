f0c0b836d2854582110b60195f4eaac83215ffc2
IAmSorryDave <212933284+IAmSorryDave@users.noreply.github.com>
2026-07-14 15:19:09 -0700

    Update init.bash

--- scripts/versioning/init.bash
+++ scripts/versioning/init.bash
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
+INITIAL_VERSION=$(basename $(git remote get-url origin))
 touch VERSION
-INITIAL_VERSION="$(basename $(git remote get-url origin)):0.0.0"
 echo $INITIAL_VERSION | tr '[:upper:]' '[:lower:]' >> VERSION
