0cce79a21bca2635d5bb8a6f99afddcf1e75782a
IAmSorryDave <212933284+IAmSorryDave@users.noreply.github.com>
2026-07-16 22:24:39 +0000

    checkpoint

--- .hooks/utilities/version_file_manager.py
+++ .hooks/utilities/version_file_manager.py
@@ -1,19 +1,34 @@
 
+from dataclasses import dataclass, field
 from pathlib import Path
 from semver.version import Version
 
+@dataclass(init=True)
 class VersionFileManager(Version):
 
   """
   A subclass of Version which creates a VERSION file if none exists then parses the file.
   """
-  
-  version_filename_encoding: str = "utf-8"
-  semantic_version_starting_label: str = "0.0.0"
 
-  def __init__(self, version_filename: str = "VERSION", alternate_directory_path: Path | None = None, *args, **kwargs):
-    super().__init__(*args, **kwargs)
-    self.version_filename, self.alternate_directory_path = version_filename, alternate_directory_path
+  alternate_directory_path: Path | None = None
+  semantic_version_starting_label: str = "0.0.0"
+  version_filename_encoding: str = "utf-8"
+  version_filename: str = "VERSION"
+  
+  args: tuple = tuple()
+  kwargs: dict = field(default_factory=dict)
+
+  def __post_init__(self):
+    if not self.filepath_exists:
+      self.write_version_file(semantic_version_label=self.semantic_version_starting_label)
+    else:
+      semantic_version_label = open(self.version_filepath,"r").read().strip()
+
+    super().__init__(self.semantic_version_starting_label, *self.args, **self.kwargs)
+
+  @property
+  def filepath_exists(self):
+    return self.version_filepath.exists()
 
   @property
   def version_filepath(self) -> Path:
@@ -51,54 +66,10 @@ class VersionFileManager(Version):
   def bump_build(self) -> None:
     super().bump_build()
     self.update_version_file
-
-  def read_and_validate_version_file(version_filepath: Path, *args, **kwargs) -> Version:
     
-    version_label = open(version_filepath,"r").read().strip()
-
-    assert super().is_valid(version_label), f"{str(version_filepath)} file contains invalid semantic version format."
-      
-    return super().parse(version_label, *args, **kwargs)
-
-  @classmethod
-  def initialize_version_file(cls, version_filepath: Path, semantic_version_starting_label: str = "0.0.0", version_filename_encoding: str = "utf-8", *args, **kwargs) -> Version:
-
-    cls.write_version_file(version_filepath=version_filepath, semantic_version_label=semantic_version_starting_label, encoding=version_filename_encoding)
-    
-    return super().parse(semantic_version_starting_label, *args, **kwargs)
-    
-  @staticmethod
-  def write_version_file(version_filepath: Path, semantic_version_label: str, encoding: str = "utf-8") -> None:
-    with version_filepath.open("w", encoding=encoding) as f:
+  def write_version_file(self, semantic_version_label: str) -> None:
+    with self.version_filepath.open("w", encoding=self.version_filename_encoding) as f:
       f.write(semantic_version_label)
-  
-  @classmethod
-  def parse(cls, version_filename: str = "VERSION", alternate_directory_path: Path | None = None, *args, **kwargs):
 
-    """
-    Parse a version file. If none found initializes new version file.
-    
-    :param version_filename: Version Filename
-    :param alternate_directory_path: Alternate Directory Containing Version File
-    :return: a new instance
-    """
-
-    version_filepath = cls(
-      version_filename=version_filename, 
-      alternate_directory_path=alternate_directory_path
-    ).version_filepath
-      
-    if version_filepath.exists():
-      version_instance = cls.read_and_validate_version_file(version_filepath, *args, **kwargs)
-    else:
-      version_instance = cls.initialize_version_file(
-        version_filepath, 
-        semantic_version_starting_label=cls.semantic_version_starting_label, 
-        version_filename_encoding=cls.version_filename_encoding, 
-        *args, **kwargs
-      )
-
-    return version_instance
-
-  
-        
+  def update_version_file(self):
+    self.write_version_file(str(self))
