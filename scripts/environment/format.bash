0a4e827e00ac99348b705ca8993adb3e58592903
IAmSorryDave <212933284+IAmSorryDave@users.noreply.github.com>
2026-08-05 18:24:02 -0700

    Clean up PROJECT_NAME extraction options in format.bash
    Removed unused options for extracting PROJECT_NAME from git remote URL.

--- scripts/environment/format.bash
+++ scripts/environment/format.bash
@@ -8,12 +8,6 @@ set +a
 # Option 1: Get PROJECT_NAME from git repository root directory name
 PROJECT_NAME=$(basename "$(git remote get-url origin)")
 
-# Option 2: Extract repository name from git remote origin URL
-# PROJECT_NAME=$(git remote get-url origin | xargs basename -s .git)
-
-# Option 3: Extract just the repo name from origin URL (handles .git suffix)
-# PROJECT_NAME=$(git config --get remote.origin.url | sed 's/.*\///' | sed 's/\.git$//')
-
 # Determine DOCKER_IMAGE_TAG based on conditions
 if [[ -n "$ALTERNATE_REPO_NAME" ]] && [[ -n "$PORT" ]]; then
     DOCKER_IMAGE_TAG="${HOST}:${PORT}/${ALTERNATE_REPO_NAME,,}${PROJECT_NAME,,}:$(cat VERSION)"
@@ -24,5 +18,3 @@ elif [[ -n "$PORT" ]]; then
 else
     DOCKER_IMAGE_TAG="${HOST}${PORT}/${GITHUB_REPOSITORY,,}:$(cat VERSION)"
 fi
-
-export DOCKER_IMAGE_TAG
