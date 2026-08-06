b2f3e97bc239728450acc1d579c996c3b7833048
IAmSorryDave <212933284+IAmSorryDave@users.noreply.github.com>
2026-08-06 09:01:42 -0700

    Update increment-patch.bash

--- scripts/versioning/increment-patch.bash
+++ scripts/versioning/increment-patch.bash
@@ -1,3 +1,11 @@
 #!/usr/bin/bash
-pre-commit run --hook-stage manual increment-version-by-patch --all-files
-git add .
+
+if [ -f VERSION ]; then
+  git update-index --assume-unchanged VERSION
+  pre-commit run --hook-stage manual increment-version-by-patch --all-files
+  git update-index --no-assume-unchanged VERSION
+else
+  pre-commit run --hook-stage manual increment-version-by-patch --all-files
+fi 
+
+git add VERSION
