4b516b2636dd96e4721518b378c5b39969462c63
IAmSorryDave <212933284+IAmSorryDave@users.noreply.github.com>
2026-08-06 06:19:32 -0700

    Update format.bash

--- scripts/environment/format.bash
+++ scripts/environment/format.bash
@@ -12,9 +12,9 @@ PROJECT_NAME=$(basename "$(git remote get-url origin)")
 if [[ -n "$ALTERNATE_REPO_NAME" ]] && [[ -n "$PORT" ]]; then
     DOCKER_IMAGE_TAG="${HOST}:${PORT}/${ALTERNATE_REPO_NAME,,}${PROJECT_NAME,,}:$(cat VERSION)"
 elif [[ -n "$ALTERNATE_REPO_NAME" ]]; then
-    DOCKER_IMAGE_TAG="${HOST}${PORT}/${ALTERNATE_REPO_NAME,,}${PROJECT_NAME,,}:$(cat VERSION)"
+    DOCKER_IMAGE_TAG="${HOST}/${ALTERNATE_REPO_NAME,,}${PROJECT_NAME,,}:$(cat VERSION)"
 elif [[ -n "$PORT" ]]; then
     DOCKER_IMAGE_TAG="${HOST}:${PORT}/${GITHUB_REPOSITORY,,}:$(cat VERSION)"
 else
-    DOCKER_IMAGE_TAG="${HOST}${PORT}/${GITHUB_REPOSITORY,,}:$(cat VERSION)"
+    DOCKER_IMAGE_TAG="${HOST}/${GITHUB_REPOSITORY,,}:$(cat VERSION)"
 fi
