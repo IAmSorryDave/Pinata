ed6e7d814e1df2141f314a45ac027b532963d4a4
IAmSorryDave <212933284+IAmSorryDave@users.noreply.github.com>
2026-08-11 12:31:14 -0700

    Fix DOCKER_IMAGE_TAG formatting in format.bash

--- scripts/environment/format.bash
+++ scripts/environment/format.bash
@@ -1,20 +1,18 @@
 #!/bin/bash
 
 # Source your .env file
-set -a
 source .env
-set +a
 
 # Option 1: Get PROJECT_NAME from git repository root directory name
 PROJECT_NAME=$(basename "$(git remote get-url origin)")
 
 # Determine DOCKER_IMAGE_TAG based on conditions
 if [[ -n "$ALTERNATE_REPO_NAME" ]] && [[ -n "$PORT" ]]; then
-    DOCKER_IMAGE_TAG="${HOST}:${PORT}/${ALTERNATE_REPO_NAME,,}${PROJECT_NAME,,}:$(cat VERSION)"
+	DOCKER_IMAGE_TAG="${HOST}:${PORT}/${ALTERNATE_REPO_NAME,,}/${PROJECT_NAME,,}:$(cat VERSION)"
 elif [[ -n "$ALTERNATE_REPO_NAME" ]]; then
-    DOCKER_IMAGE_TAG="${HOST}/${ALTERNATE_REPO_NAME,,}${PROJECT_NAME,,}:$(cat VERSION)"
+	DOCKER_IMAGE_TAG="${HOST}/${ALTERNATE_REPO_NAME,,}/${PROJECT_NAME,,}:$(cat VERSION)"
 elif [[ -n "$PORT" ]]; then
-    DOCKER_IMAGE_TAG="${HOST}:${PORT}/${GITHUB_REPOSITORY,,}:$(cat VERSION)"
+	DOCKER_IMAGE_TAG="${HOST}:${PORT}/${GITHUB_REPOSITORY,,}:$(cat VERSION)"
 else
-    DOCKER_IMAGE_TAG="${HOST}/${GITHUB_REPOSITORY,,}:$(cat VERSION)"
+	DOCKER_IMAGE_TAG="${HOST}/${GITHUB_REPOSITORY,,}:$(cat VERSION)"
 fi
