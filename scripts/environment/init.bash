c1c366bf2746ecdf471f8ab8750d7b64e83d646c
IAmSorryDave <212933284+IAmSorryDave@users.noreply.github.com>
2026-08-11 12:32:01 -0700

    Ensure DOCKER_IMAGE_TAG is defined after format.bash
    Added check to ensure DOCKER_IMAGE_TAG is set after running format.bash.

--- scripts/environment/init.bash
+++ scripts/environment/init.bash
@@ -5,8 +5,15 @@ source .env
 
 # Check if PORT or ALTERNATE_REPO_NAME is defined
 if [[ -n "$PORT" || -n "$ALTERNATE_REPO_NAME" ]]; then
-    echo "PORT or ALTERNATE_REPO_NAME is defined. Running format.bash..."
-    source scripts/environment/format.bash
+	echo "PORT or ALTERNATE_REPO_NAME is defined. Running format.bash..."
+	source scripts/environment/format.bash
 else
-    echo "Neither PORT nor ALTERNATE_REPO_NAME is defined. Skipping format.bash."
+	echo "Neither PORT nor ALTERNATE_REPO_NAME is defined. Skipping format.bash."
+	DOCKER_IMAGE_TAG="${HOST}/${GITHUB_REPOSITORY,,}:$(cat VERSION)"
 fi
+
+# Ensure DOCKER_IMAGE_TAG exists after format.bash too
+test -n "${DOCKER_IMAGE_TAG}" || {
+	echo "DOCKER_IMAGE_TAG is empty"
+	exit 1
+}
