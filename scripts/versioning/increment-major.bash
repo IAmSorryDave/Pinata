a0da36ccbabccc07e761528109dbd8925956c5c3
IAmSorryDave <212933284+IAmSorryDave@users.noreply.github.com>
2026-08-11 12:38:08 -0700

    Refactor increment-major.bash for versioning process

--- scripts/versioning/increment-major.bash
+++ scripts/versioning/increment-major.bash
@@ -1,11 +1,11 @@
 #!/usr/bin/bash
 
 if [ -f VERSION ]; then
-  git update-index --assume-unchanged VERSION
-  pre-commit run --hook-stage manual increment-version-by-major --all-files
-  git update-index --no-assume-unchanged VERSION
+	git update-index --assume-unchanged VERSION
+	pre-commit run --hook-stage manual increment-version-by-major --all-files
+	git update-index --no-assume-unchanged VERSION
 else
-  pre-commit run --hook-stage manual increment-version-by-major --all-files
-fi 
+	pre-commit run --hook-stage manual increment-version-by-major --all-files
+fi
 
 git add VERSION
