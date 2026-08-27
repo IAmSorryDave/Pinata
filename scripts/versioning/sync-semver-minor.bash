1c24a6b5c5457d8d3dd252f9967126410514f5ce
IAmSorryDave <212933284+IAmSorryDave@users.noreply.github.com>
2026-08-27 07:19:21 -0700

    Update sync-semver-minor.bash for test directory changes

--- scripts/versioning/sync-semver-minor.bash
+++ scripts/versioning/sync-semver-minor.bash
@@ -2,7 +2,8 @@
 
 if [ -f VERSION ]; then
 	git update-index --assume-unchanged VERSION
-	for i in $(seq $(find ./tests -maxdepth 1 -mindepth 1 -type d | wc -l)); do
+	pre-commit run --hook-stage manual reset-semver-minor --all-files
+	for i in $(seq $(find ./bats/tests -maxdepth 1 -mindepth 1 -type d | wc -l)); do
 		pre-commit run --hook-stage manual increment-version-by-minor --all-files
 	done
 	git update-index --no-assume-unchanged VERSION
