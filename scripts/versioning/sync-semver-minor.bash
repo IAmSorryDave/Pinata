db8d2d1caad065c20142bcb35228e6f041af2162
IAmSorryDave <212933284+IAmSorryDave@users.noreply.github.com>
2026-08-27 07:04:09 -0700

    Update and rename increment-minor.bash to sync-semver-minor.bash

--- scripts/versioning/sync-semver-minor.bash
+++ scripts/versioning/sync-semver-minor.bash
@@ -2,7 +2,9 @@
 
 if [ -f VERSION ]; then
 	git update-index --assume-unchanged VERSION
-	pre-commit run --hook-stage manual increment-version-by-minor --all-files
+	for i in $(seq $(find ./tests -maxdepth 1 -mindepth 1 -type d | wc -l)); do
+		pre-commit run --hook-stage manual increment-version-by-minor --all-files
+	done
 	git update-index --no-assume-unchanged VERSION
 else
 	pre-commit run --hook-stage manual increment-version-by-minor --all-files
