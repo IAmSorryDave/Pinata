e6a7bb6fc470107c276e10e07dc2be0bca761899
IAmSorryDave <212933284+IAmSorryDave@users.noreply.github.com>
2026-08-27 08:41:56 -0700

    Fix path to format.bash in init.bash

--- scripts/environment/init.bash
+++ scripts/environment/init.bash
@@ -6,7 +6,7 @@ source .env
 # Check if PORT or ALTERNATE_REPO_NAME is defined
 if [[ -n "$PORT" || -n "$ALTERNATE_REPO_NAME" ]]; then
 	echo "PORT or ALTERNATE_REPO_NAME is defined. Running format.bash..."
-	source scripts/environment/format.bash
+	source .scripts/environment/format.bash
 else
 	echo "Neither PORT nor ALTERNATE_REPO_NAME is defined. Skipping format.bash."
 	DOCKER_IMAGE_TAG="${HOST}/${GITHUB_REPOSITORY,,}:$(cat VERSION)"
